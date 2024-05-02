1ce67b3c6bd2709e07f044b2e16d33fecd37063f
manojvsp <gmanojece2021@gmail.com>
2024-05-02 21:33:40 +0530

    created routes with react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import Signin from './pages/Signin';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/profile' element={<Profile/>} />
+        <Route path='/signin' element={<Signin/>} />
+        <Route path='/signup' element={<SignUp/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App
+export default App;
